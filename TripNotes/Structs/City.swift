da65b97d5466981620cac08ebce3ebc8721f2fb0
John Park <parkjmjohn@gmail.com>
2017-12-09 13:02:57 -0500

    Created Weather class

---
+++ TripNotes/Structs/City.swift
@@ -0,0 +1,23 @@
+//
+//  City.swift
+//  TripNotes
+//
+//  Created by John Park on 11/30/17.
+//  Copyright © 2017 John Park. All rights reserved.
+//
+import Foundation
+
+struct City {
+    
+    // MARK: title
+    var label: String
+    var notes: String 
+    
+    // MARK: init
+    init(label: String, notes: String) {
+        self.label = label
+        self.notes = notes
+    }
+    
+}
+
---
+++ TripNotes/Structs/Weather.swift
@@ -0,0 +1,378 @@
+//
+//  Weather.swift
+//  TripNotes
+//
+//  Created by John Park on 12/9/17.
+//  Copyright © 2017 John Park. All rights reserved.
+//
+
+import UIKit
+
+struct Weather {
+    var time: String
+    var hour0: String
+    var hour0temp: String
+    var hour0rain: String
+    var hour0text: String
+    var hour0img: UIImage
+    var hour1: String
+    var hour1temp: String
+    var hour1rain: String
+    var hour1text: String
+    var hour1img: UIImage
+    var hour2: String
+    var hour2temp: String
+    var hour2rain: String
+    var hour2text: String
+    var hour2img: UIImage
+    var hour3: String
+    var hour3temp: String
+    var hour3rain: String
+    var hour3text: String
+    var hour3img: UIImage
+    var hour4: String
+    var hour4temp: String
+    var hour4rain: String
+    var hour4text: String
+    var hour4img: UIImage
+    var hour5: String
+    var hour5temp: String
+    var hour5rain: String
+    var hour5text: String
+    var hour5img: UIImage
+    var hour6: String
+    var hour6temp: String
+    var hour6rain: String
+    var hour6text: String
+    var hour6img: UIImage
+    var hour7: String
+    var hour7temp: String
+    var hour7rain: String
+    var hour7text: String
+    var hour7img: UIImage
+    var hour8: String
+    var hour8temp: String
+    var hour8rain: String
+    var hour8text: String
+    var hour8img: UIImage
+    var hour9: String
+    var hour9temp: String
+    var hour9rain: String
+    var hour9text: String
+    var hour9img: UIImage
+    var hour10: String
+    var hour10temp: String
+    var hour10rain: String
+    var hour10text: String
+    var hour10img: UIImage
+    var hour11: String
+    var hour11temp: String
+    var hour11rain: String
+    var hour11text: String
+    var hour11img: UIImage
+    var hour12: String
+    var hour12temp: String
+    var hour12rain: String
+    var hour12text: String
+    var hour12img: UIImage
+    var hour13: String
+    var hour13temp: String
+    var hour13rain: String
+    var hour13text: String
+    var hour13img: UIImage
+    var hour14: String
+    var hour14temp: String
+    var hour14rain: String
+    var hour14text: String
+    var hour14img: UIImage
+    var hour15: String
+    var hour15temp: String
+    var hour15rain: String
+    var hour15text: String
+    var hour15img: UIImage
+    var hour16: String
+    var hour16temp: String
+    var hour16rain: String
+    var hour16text: String
+    var hour16img: UIImage
+    var hour17: String
+    var hour17temp: String
+    var hour17rain: String
+    var hour17text: String
+    var hour17img: UIImage
+    var hour18: String
+    var hour18temp: String
+    var hour18rain: String
+    var hour18text: String
+    var hour18img: UIImage
+    var hour19: String
+    var hour19temp: String
+    var hour19rain: String
+    var hour19text: String
+    var hour19img: UIImage
+    var hour20: String
+    var hour20temp: String
+    var hour20rain: String
+    var hour20text: String
+    var hour20img: UIImage
+    var hour21: String
+    var hour21temp: String
+    var hour21rain: String
+    var hour21text: String
+    var hour21img: UIImage
+    var hour22: String
+    var hour22temp: String
+    var hour22rain: String
+    var hour22text: String
+    var hour22img: UIImage
+    var hour23: String
+    var hour23temp: String
+    var hour23rain: String
+    var hour23text: String
+    var hour23img: UIImage
+    
+    init(time: String,
+         hour0: String,
+         hour0temp: String,
+         hour0rain: String,
+         hour0text: String,
+         hour0img: UIImage,
+         hour1: String,
+         hour1temp: String,
+         hour1rain: String,
+         hour1text: String,
+         hour1img: UIImage,
+         hour2: String,
+         hour2temp: String,
+         hour2rain: String,
+         hour2text: String,
+         hour2img: UIImage,
+         hour3: String,
+         hour3temp: String,
+         hour3rain: String,
+         hour3text: String,
+         hour3img: UIImage,
+         hour4: String,
+         hour4temp: String,
+         hour4rain: String,
+         hour4text: String,
+         hour4img: UIImage,
+         hour5: String,
+         hour5temp: String,
+         hour5rain: String,
+         hour5text: String,
+         hour5img: UIImage,
+         hour6: String,
+         hour6temp: String,
+         hour6rain: String,
+         hour6text: String,
+         hour6img: UIImage,
+         hour7: String,
+         hour7temp: String,
+         hour7rain: String,
+         hour7text: String,
+         hour7img: UIImage,
+         hour8: String,
+         hour8temp: String,
+         hour8rain: String,
+         hour8text: String,
+         hour8img: UIImage,
+         hour9: String,
+         hour9temp: String,
+         hour9rain: String,
+         hour9text: String,
+         hour9img: UIImage,
+         hour10: String,
+         hour10temp: String,
+         hour10rain: String,
+         hour10text: String,
+         hour10img: UIImage,
+         hour11: String,
+         hour11temp: String,
+         hour11rain: String,
+         hour11text: String,
+         hour11img: UIImage,
+         hour12: String,
+         hour12temp: String,
+         hour12rain: String,
+         hour12text: String,
+         hour12img: UIImage,
+         hour13: String,
+         hour13temp: String,
+         hour13rain: String,
+         hour13text: String,
+         hour13img: UIImage,
+         hour14: String,
+         hour14temp: String,
+         hour14rain: String,
+         hour14text: String,
+         hour14img: UIImage,
+         hour15: String,
+         hour15temp: String,
+         hour15rain: String,
+         hour15text: String,
+         hour15img: UIImage,
+         hour16: String,
+         hour16temp: String,
+         hour16rain: String,
+         hour16text: String,
+         hour16img: UIImage,
+         hour17: String,
+         hour17temp: String,
+         hour17rain: String,
+         hour17text: String,
+         hour17img: UIImage,
+         hour18: String,
+         hour18temp: String,
+         hour18rain: String,
+         hour18text: String,
+         hour18img: UIImage,
+         hour19: String,
+         hour19temp: String,
+         hour19rain: String,
+         hour19text: String,
+         hour19img: UIImage,
+         hour20: String,
+         hour20temp: String,
+         hour20rain: String,
+         hour20text: String,
+         hour20img: UIImage,
+         hour21: String,
+         hour21temp: String,
+         hour21rain: String,
+         hour21text: String,
+         hour21img: UIImage,
+         hour22: String,
+         hour22temp: String,
+         hour22rain: String,
+         hour22text: String,
+         hour22img: UIImage,
+         hour23: String,
+         hour23temp: String,
+         hour23rain: String,
+         hour23text: String,
+         hour23img: UIImage) {
+        self.time = time
+        self.hour0 = hour0
+        self.hour0temp = hour0temp
+        self.hour0rain = hour0rain
+        self.hour0text = hour0text
+        self.hour0img = hour0img
+        self.hour1 = hour1
+        self.hour1temp = hour1temp
+        self.hour1rain = hour1rain
+        self.hour1text = hour1text
+        self.hour1img = hour1img
+        self.hour2 = hour2
+        self.hour2temp = hour2temp
+        self.hour2rain = hour2rain
+        self.hour2text = hour2text
+        self.hour2img = hour2img
+        self.hour3 = hour3
+        self.hour3temp = hour3temp
+        self.hour3rain = hour3rain
+        self.hour3text = hour3text
+        self.hour3img = hour3img
+        self.hour4 = hour4
+        self.hour4temp = hour4temp
+        self.hour4rain = hour4rain
+        self.hour4text = hour4text
+        self.hour4img = hour4img
+        self.hour5 = hour5
+        self.hour5temp = hour5temp
+        self.hour5rain = hour5rain
+        self.hour5text = hour5text
+        self.hour5img = hour5img
+        self.hour6 = hour6
+        self.hour6temp = hour6temp
+        self.hour6rain = hour6rain
+        self.hour6text = hour6text
+        self.hour6img = hour6img
+        self.hour7 = hour7
+        self.hour7temp = hour7temp
+        self.hour7rain = hour7rain
+        self.hour7text = hour7text
+        self.hour7img = hour7img
+        self.hour8 = hour8
+        self.hour8temp = hour8temp
+        self.hour8rain = hour8rain
+        self.hour8text = hour8text
+        self.hour8img = hour8img
+        self.hour9 = hour9
+        self.hour9temp = hour9temp
+        self.hour9rain = hour9rain
+        self.hour9text = hour9text
+        self.hour9img = hour9img
+        self.hour10 = hour10
+        self.hour10temp = hour10temp
+        self.hour10rain = hour10rain
+        self.hour10text = hour10text
+        self.hour10img = hour10img
+        self.hour11 = hour11
+        self.hour11temp = hour11temp
+        self.hour11rain = hour11rain
+        self.hour11text = hour11text
+        self.hour11img = hour11img
+        self.hour12 = hour12
+        self.hour12temp = hour12temp
+        self.hour12rain = hour12rain
+        self.hour12text = hour12text
+        self.hour12img = hour12img
+        self.hour13 = hour13
+        self.hour13temp = hour13temp
+        self.hour13rain = hour13rain
+        self.hour13text = hour13text
+        self.hour13img = hour13img
+        self.hour14 = hour14
+        self.hour14temp = hour14temp
+        self.hour14rain = hour14rain
+        self.hour14text = hour14text
+        self.hour14img = hour14img
+        self.hour15 = hour15
+        self.hour15temp = hour15temp
+        self.hour15rain = hour15rain
+        self.hour15text = hour15text
+        self.hour15img = hour15img
+        self.hour16 = hour16
+        self.hour16temp = hour16temp
+        self.hour16rain = hour16rain
+        self.hour16text = hour16text
+        self.hour16img = hour16img
+        self.hour17 = hour17
+        self.hour17temp = hour17temp
+        self.hour17rain = hour17rain
+        self.hour17text = hour17text
+        self.hour17img = hour17img
+        self.hour18 = hour18
+        self.hour18temp = hour18temp
+        self.hour18rain = hour18rain
+        self.hour18text = hour18text
+        self.hour18img = hour18img
+        self.hour19 = hour19
+        self.hour19temp = hour19temp
+        self.hour19rain = hour19rain
+        self.hour19text = hour19text
+        self.hour19img = hour19img
+        self.hour20 = hour20
+        self.hour20temp = hour20temp
+        self.hour20rain = hour20rain
+        self.hour20text = hour20text
+        self.hour20img = hour20img
+        self.hour21 = hour21
+        self.hour21temp = hour21temp
+        self.hour21rain = hour21rain
+        self.hour21text = hour21text
+        self.hour21img = hour21img
+        self.hour22 = hour22
+        self.hour22temp = hour22temp
+        self.hour22rain = hour22rain
+        self.hour22text = hour22text
+        self.hour22img = hour22img
+        self.hour23 = hour23
+        self.hour23temp = hour23temp
+        self.hour23rain = hour23rain
+        self.hour23text = hour23text
+        self.hour23img = hour23img
+    }
+    
+}
